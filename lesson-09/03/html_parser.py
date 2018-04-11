67ed06c0731297cfa916a5cec53922536e666bae
Minimum <s5550055@yahoo.com.tw>
2018-04-11 10:31:00 +0000

    finished html parser

--- lesson-09/03/html_parser.py
+++ lesson-09/03/html_parser.py
@@ -1,15 +1,64 @@
 #!/usr/bin/env python3
 
+import sys
 from html.parser import HTMLParser
+from html.entities import name2codepoint
+from pathlib import Path
 
 
-with open('config.json', 'r') as myfile:
-    config = load(myfile)
-    print(config)
+class MyHTMLParser(HTMLParser):
+    __in_tbody = False
+    __in_td_th = False
+    __in_tr = False
+    __print = False
 
-    for index, account in enumerate(config['account']):
-        print('account', index) 
-        for key, value in account.items():
-            print('{}: {}'.format(key, value))
+    def handle_starttag(self, tag, attrs):
+        if tag == 'tbody':
+            self.__in_tbody = True
+        elif tag == 'tr':
+            self.__in_tr = True
+        elif tag in ('td', 'th'):
+            self.__in_td_th = True
 
-## you can re-run to see the property of dict
+        for attr in attrs:
+            if 'print_hide' in attr[1]:
+                self.__print = True
+
+    def handle_endtag(self, tag):
+        self.__print = False
+        if tag == 'tbody':
+            self.__in_tbody = False
+        elif tag == 'tr':
+            self.__in_tr = False
+            if self.__in_tbody:
+                print('')
+        elif tag in ('td', 'th'):
+            self.__in_td_th = False
+
+    def handle_data(self, data):
+        if self.__in_tbody and self.__in_td_th:
+            data = data.strip()
+            if self.__print and data and data not in ('查詢'):
+                print(data, end = ',')
+
+    def handle_entityref(self, name):
+        c = chr(name2codepoint[name])
+        print("Named ent:", c)
+
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            c = chr(int(name[1:], 16))
+        else:
+            c = chr(int(name))
+        print("Num ent  :", c)
+
+
+assert sys.version_info >= (3, 5)
+
+
+if __name__ == '__main__':
+    myfile = Path('rate.html')
+    content = myfile.read_text()
+
+    parser = MyHTMLParser()
+    parser.feed(content)
