63b901ea0d3ed1153174133117f746f55177375f
Minimum <s5550055@yahoo.com.tw>
2018-04-11 10:48:14 +0000

    html parser version 2

---
+++ lesson-09/03/html_parser_v2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import sys
+from html.parser import HTMLParser
+from html.entities import name2codepoint
+from pathlib import Path
+
+
+class MyHTMLParser(HTMLParser):
+    candidate = [
+        '.tbody.tr.td',
+        '.tbody.tr.td.div.div'
+    ]
+    __current_path = ''
+    __print = False
+
+    def handle_starttag(self, tag, attrs):
+        self.__current_path += '.' + tag
+        for attr in attrs:
+            if 'print_hide' in attr[1]:
+                self.__print = True
+
+    def handle_endtag(self, tag):
+        self.__print = False
+        if self.__current_path.endswith('.tbody.tr'):
+            print('')
+        self.__current_path = self.__current_path[:-len(tag)-1]
+
+    def handle_data(self, data):
+        for template in self.candidate:
+            if self.__current_path.endswith(template):
+                data = data.strip()
+                if self.__print and data and data not in ('查詢'):
+                    print(data, end = ',')
+
+    def handle_entityref(self, name):
+        c = chr(name2codepoint[name])
+        print("Named ent:", c)
+
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            c = chr(int(name[1:], 16))
+        else:
+            c = chr(int(name))
+        print("Num ent  :", c)
+
+
+assert sys.version_info >= (3, 5)
+
+
+if __name__ == '__main__':
+    myfile = Path('rate.html')
+    content = myfile.read_text()
+
+    parser = MyHTMLParser()
+    parser.feed(content)
