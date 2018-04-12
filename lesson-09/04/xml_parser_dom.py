7da45a0b7898d1834eabe0e6bd29278fa2fa31f9
Minimum <s5550055@yahoo.com.tw>
2018-04-12 07:21:15 +0000

    Add xml parser

---
+++ lesson-09/04/xml_parser_dom.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from xml.dom.minidom import parse
+
+
+def parse_appledaily_news(file_: str):
+    dom = parse(file_)
+    root = dom.documentElement
+    items = [x for x in root.getElementsByTagName('channel')[0].childNodes \
+        if x.nodeType is root.ELEMENT_NODE and x.nodeName == 'item']
+
+    ###################################
+    # sub function
+    ###################################
+    def parse_item(item):
+        for node in item.childNodes:
+            if node.nodeName == 'title':
+                title = parse_title(node)
+            elif node.nodeName == 'link':
+                link = parse_link(node)
+        return title, link
+
+    def parse_title(node) -> str:
+        return parse_data(node)
+
+    def parse_link(node) -> str:
+        return parse_data(node)
+
+    def parse_data(node) -> str:
+        for n in node.childNodes:
+            if n.nodeType in (root.TEXT_NODE, root.CDATA_SECTION_NODE):
+                return n.data
+    ## end of sub function
+
+    news = [parse_item(item) for item in items]
+    return news
+
+
+if __name__ == '__main__':
+    news = parse_appledaily_news('news.xml')
+    print(*news, sep = '\n')
+
---
+++ lesson-09/04/xml_parser_sax.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from xml.sax import parse, ContentHandler
+
+class AppleDailyNewsHandler(ContentHandler):
+
+    __title = ''
+    __link = ''
+    __current_path = ''
+
+    def startElement(self, tag, attrs):
+        self.__current_path += '.' + tag
+
+    def endElement(self, tag):
+        if tag == 'item':
+            print((self.__title, self.__link))
+        self.__current_path = self.__current_path[:-len(tag)-1]
+
+    def characters(self, data):
+        if self.__current_path.endswith('.item.title'):
+            self.__title = data
+        elif self.__current_path.endswith('.item.link'):
+            self.__link = data
+
+
+if __name__ == '__main__':
+    parse('news.xml', AppleDailyNewsHandler())
+
